46c18f5bf2eeb73a4fe6aad55a6fec31ea91af61
saisowmyachandrapati <chsaisowmya02@gmail.com>
2024-09-20 16:00:07 +0530

    Add files via upload

---
+++ KafkaDatabase-1/src/main/java/com/kafkadatabase1/consumer/KafkaDatabase1Application.java
@@ -0,0 +1,13 @@
+package com.kafkadatabase1.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDatabase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDatabase1Application.class, args);
+	}
+
+}
---
+++ KafkaDatabase-1/src/test/java/com/kafkadatabase1/consumer/KafkaDatabase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkadatabase1.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDatabase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
